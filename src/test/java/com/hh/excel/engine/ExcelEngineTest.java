24195b6c8f28529f6842e0625041615900377925
H-hao <z211063515@outlook.com>
2017-07-30 11:22:40 +0800

    添加：测试（方法）类，xml文件（还没有实质内容）

--- src/test/java/com/hh/excel/engine/ExcelEngineTest.java
+++ src/test/java/com/hh/excel/engine/ExcelEngineTest.java
@@ -1,5 +1,10 @@
 package com.hh.excel.engine;
 
+import com.hh.excel.engine.config.EngineConfiguration;
+import com.hh.excel.engine.config.ExcelConfiguration;
+import com.hh.excel.engine.core.engine.DefaultExcelEngine;
+import com.hh.excel.engine.core.engine.ExcelEngine;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -7,6 +12,13 @@ import org.junit.Test;
  * Created by 黄浩 on 2017/7/30 0030.
  */
 public class ExcelEngineTest {
+	@Before
+	public void setUp() throws Exception {
+		ExcelConfiguration excelConfiguration = new ExcelConfiguration();
+		EngineConfiguration engineConfiguration = new EngineConfiguration();
+		ExcelEngine excelEngine = new DefaultExcelEngine(engineConfiguration, excelConfiguration);
+	}
+
 	@Test
 	public void testImport(){
 
@@ -14,6 +26,16 @@ public class ExcelEngineTest {
 
 	@Test
 	public void testExport(){
+
+	}
+
+	@Test
+	public void testImportByWeb(){
+
+	}
+
+	@Test
+	public void testExportByWeb(){
 
 	}
 }
---
+++ src/test/java/com/hh/excel/engine/ExcelEngineWithSpringTest.java
@@ -0,0 +1,34 @@
+package com.hh.excel.engine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 测试与 spring 集成后的导入导出
+ * Created by 黄浩 on 2017/7/30 0030.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class ExcelEngineWithSpringTest {
+	@Test
+	public void testImport(){
+
+	}
+
+	@Test
+	public void testExport(){
+
+	}
+
+	@Test
+	public void testImportByWeb(){
+
+	}
+
+	@Test
+	public void testExportByWeb(){
+
+	}
+}
